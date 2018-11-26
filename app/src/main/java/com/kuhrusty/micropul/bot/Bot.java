55d0f51464fc8fea03af9b9a78c4e82c52410777
Rusty Ballinger <rusty.ballinger@gmail.com>
2018-11-26 00:08:59 -0800

    Initial checkin.

---
+++ app/src/main/java/com/kuhrusty/micropul/bot/Bot.java
@@ -0,0 +1,39 @@
+package com.kuhrusty.micropul.bot;
+
+import com.kuhrusty.micropul.MoveListener;
+import com.kuhrusty.micropul.model.Board;
+import com.kuhrusty.micropul.model.Opponent;
+import com.kuhrusty.micropul.model.Player;
+
+public interface Bot {
+    /**
+     * Returns the name which will be displayed in the start-game screen, and as
+     * the player name during games.
+     */
+    String getName();
+
+    /**
+     * Returns a brief description of the bot for display in the start-game
+     * screen.
+     */
+    String getDescription();
+
+    /**
+     * Called when the bot should make a move.  When it decides what it's going
+     * to do, it should call listener.playTile(), drawTile(), placeStone(), or
+     * concede(), passing the Player instance which was passed in.  Everything
+     * which gets passed in is a copy and can be modified.
+     *
+     * <p>Note that this will be called on a non-UI thread, so you don't have
+     * to worry about doing that yourself.  When you decide on your move and
+     * notify the listener, the listener will take care of notifying the UI
+     * thread.</p>
+     *
+     * @param self information about the bot player.
+     * @param opponent information about the opponent.
+     * @param board the board state.
+     * @param listener the listener who should be notified when the move is
+     *                 decided on.
+     */
+    void takeTurn(Player self, Opponent opponent, Board board, MoveListener listener);
+}
---
+++ app/src/main/java/com/kuhrusty/micropul/bot/DumbBot.java
@@ -0,0 +1,64 @@
+package com.kuhrusty.micropul.bot;
+
+import android.content.res.Resources;
+
+import com.kuhrusty.micropul.MoveListener;
+import com.kuhrusty.micropul.R;
+import com.kuhrusty.micropul.model.Board;
+import com.kuhrusty.micropul.model.Opponent;
+import com.kuhrusty.micropul.model.Player;
+import com.kuhrusty.micropul.model.Tile;
+
+/**
+ * The dumbest possible AI: makes the first move it can find.
+ */
+public class DumbBot implements Bot {
+
+    public DumbBot(Resources res) {
+        this.res = res;
+    }
+
+    @Override
+    public String getName() {
+        return res.getString(R.string.dumbbot_name);
+    }
+
+    @Override
+    public String getDescription() {
+        return res.getString(R.string.dumbbot_descr);
+    }
+
+    @Override
+    public void takeTurn(Player self, Opponent opponent, Board board, MoveListener listener) {
+        if (self.getTilesInHand() == 0) {
+            if (self.getTilesInSupply() > 0) {
+                listener.drawTile(self);
+            } else {
+                listener.concede(self, null);
+            }
+            return;
+        }
+        for (Tile tile : self.getTiles()) {
+            for (int rotation = 0; rotation < 4; ++rotation) {
+                for (int ypos = board.getHeight(); ypos >= -1; --ypos) {
+                    for (int xpos = -1; xpos <= board.getWidth(); ++xpos) {
+                        if (board.isValidPlay(tile, xpos, ypos, null)) {
+                            listener.playTile(self, tile, xpos, ypos);
+                            return;
+                        }
+                    }
+                }
+                //  still here, so try rotating it & trying again.
+                tile.rotateRight();
+            }
+        }
+        //  still here, so try drawing a new tile.
+        if (self.getTilesInSupply() > 0) {
+            listener.drawTile(self);
+        } else {
+            listener.concede(self, null);  //  gurkk!!
+        }
+    }
+
+    private Resources res;
+}
